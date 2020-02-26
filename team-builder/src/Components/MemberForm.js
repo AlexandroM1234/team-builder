1a187d0ac70b95cf82dd51388de96ba105d348ae
Alex Martinez <alexmartinez0071234@gmail.com>
2020-02-26 17:06:40 -0500

    made the form work for the name email and role and its render to the screen correctly

--- team-builder/src/Components/MemberForm.js
+++ team-builder/src/Components/MemberForm.js
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 const MemberForm = props => {
+  const [teamMembers, setTeamMembers] = useState({
+    name: "",
+    email: "",
+    role: ""
+  });
+
+  const handleChanges = e => {
+    setTeamMembers({ ...teamMembers, [e.target.name]: e.target.value });
+  };
+
+  const formSubmit = e => {
+    e.preventDefault();
+    props.addNewMember(teamMembers);
+    setTeamMembers({ name: "", email: "", role: "" });
+  };
+
   return (
-    <div>
-      <p> Test</p>
-    </div>
+    <form onSubmit={formSubmit}>
+      <label htmlFor="name">Member Name</label>
+      <input
+        id="name"
+        name="name"
+        type="text"
+        onChange={handleChanges}
+        placeholder="name"
+        value={teamMembers.name}
+      />
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={handleChanges}
+        placeholder="email"
+        value={teamMembers.email}
+      />
+      <input
+        id="role"
+        name="role"
+        type="role"
+        onChange={handleChanges}
+        placeholder="role"
+        value={teamMembers.role}
+      />
+      <button type="submit">Add Member</button>
+    </form>
   );
 };
 
